6687452c9b715aff026938a6009a4874ff6fa110
yosepjeon <enekelx1@naver.com>
2021-04-06 19:35:33 +0900

    project init

---
+++ src/main/java/com/yosep/batch/YosepSpringBatch2021Application.java
@@ -0,0 +1,12 @@
+package com.yosep.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YosepSpringBatch2021Application {
+    public static void main(String[] args) {
+        SpringApplication.run(YosepSpringBatch2021Application.class, args);
+    }
+
+}
