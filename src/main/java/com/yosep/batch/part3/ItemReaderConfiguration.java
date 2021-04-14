9224b0c5eee81eb5f4c8c4c7bc16cfb9a98435b1
yosepjeon <enekelx1@naver.com>
2021-04-14 18:13:39 +0900

    JDBC 데이터 읽기 Complete

--- src/main/java/com/yosep/batch/part3/ItemReaderConfiguration.java
+++ src/main/java/com/yosep/batch/part3/ItemReaderConfiguration.java
@@ -7,6 +7,8 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -15,6 +17,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -24,11 +27,14 @@ import java.util.stream.Collectors;
 public class ItemReaderConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     public ItemReaderConfiguration(JobBuilderFactory jobBuilderFactory,
-                                   StepBuilderFactory stepBuilderFactory) {
+                                   StepBuilderFactory stepBuilderFactory,
+                                   DataSource dataSource) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
+        this.dataSource = dataSource;
     }
 
     @Bean
@@ -37,6 +43,7 @@ public class ItemReaderConfiguration {
                 .incrementer(new RunIdIncrementer())
                 .start(this.customItemReaderStep())
                 .next(csvFileStep())
+                .next(jdbcStep())
                 .build();
     }
 
@@ -54,6 +61,15 @@ public class ItemReaderConfiguration {
         return stepBuilderFactory.get("csvFileStep")
                 .<Person, Person>chunk(10)
                 .reader(this.csvFileItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcStep() throws Exception {
+        return stepBuilderFactory.get("jdbcStep")
+                .<Person, Person>chunk(10)
+                .reader(jdbcCursorItemReader())
                 .writer(itemWriter())
                 .build();
     }
@@ -86,6 +102,18 @@ public class ItemReaderConfiguration {
                 .build();
 
         itemReader.afterPropertiesSet(); // itemReader에서 필요한 필수 설정값이 정상적으로 설정되었는지 검증하는 코드
+
+        return itemReader;
+    }
+
+    private JdbcCursorItemReader<Person> jdbcCursorItemReader() throws Exception {
+        JdbcCursorItemReader<Person> itemReader = new JdbcCursorItemReaderBuilder<Person>()
+                .name("jdbcCursorItemReader")
+                .dataSource(dataSource)
+                .sql("select id, name, age, address from person")
+                .rowMapper(((rs, rowNum) -> new Person(rs.getInt(1),rs.getString(2),rs.getString(3),rs.getString(4))))
+                .build();
+        itemReader.afterPropertiesSet();
 
         return itemReader;
     }
